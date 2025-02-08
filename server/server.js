d779c59b9995f3515632c92b38ad0a1e9fd7dbbf
sharon <martinezsharonr@gmail.com>
2025-02-07 20:08:02 -0800

    Installed dependencies for server, api and database. Installed Node v20 for syntax errors

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 
-const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
 const PORT = process.env.PORT || 5000;
+// const connectDB = require("./config/db")
+
+require("dotenv").config();
+
+const app = express();
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+
+// Connect to the Mongo DB /need to make changes once we have a mongo database
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactcms");
+// connectDB(); 
+
 
 // Basic route
 app.get('/', (req, res) => {
